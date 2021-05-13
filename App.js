5230a7e2a7bbb3d6809988f4ed31fe36e8b7d735
adasilvapdev <adasilvapdev@gmail.com>
2021-05-12 22:11:07 -0400

    Added the tab bar icon in bottom tab navigator

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import { Image } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import CoinsStack from './src/components/coins/CoinsStack'
+
+const Tabs = createBottomTabNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+
+      <Tabs.Navigator>
+        <Tabs.Screen
+          name='Coins'
+          component={CoinsStack}
+          options={{
+            activeTintColor: '#e91e63',
+            tabBarIcon: ({ size, color }) => (
+              <Image
+                style={{
+                  tintColor: color,
+                  width: size,
+                  height: size,
+                }}
+                source={require('./src/assets/bank.png')}
+              />
+            )
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   )
 }
